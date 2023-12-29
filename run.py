46d6b9fb2bf391881f687a726a9a8405574063bb
Graciekan21 <>
2023-12-29 07:46:16 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,19 +1,17 @@
-# Import module
+#Import module
 import tkinter
 import tkinter.messagebox
+
+#The app title
+window = tkinter.Tk()
+window.title('Bakers-to-do-list')
+
+#Create a Frame for the Tasks
+task_frame = tkinter.task_frame(window)
+task_frame.pack()
 
 # Function to capture a task
 def capture_task():
     task = input('Enter your task: ')
     task_list.append(task)
     print('Task captured successfully!')
-
-# The app title
-window = tkinter.Tk()
-window.title('Bakers-to-do-list')
-
-# Create a Frame for the Tasks
-task_frame = tkinter.task_frame(window)
-task_frame.pack()
-
-window.mainloop()
