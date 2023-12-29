64298dd63bd5189d7d41aacf763d70cb68f92411
Graciekan21 <>
2023-12-29 06:58:15 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 # Import module
 from tkinter import *
-from tkinter import.messagebox
+from tkinter import messagebox
 
 # Function to capture a task
 def capture_task():
     task = input('Enter your task: ')
     task_list.append(task)
     print('Task captured successfully!')
+
 # The main
 window = tkinter.tk()
 window.title('Baking-Chronicles-to-do-list')
