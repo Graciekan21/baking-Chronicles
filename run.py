bad80da714d0e8b8baaceb3e164d549b7cc205dc
Graciekan21 <>
2023-12-29 07:19:09 +0000

    Add changes and create a frame for the tasks

--- run.py
+++ run.py
@@ -8,8 +8,12 @@ def capture_task():
     task_list.append(task)
     print('Task captured successfully!')
 
-# The main
-window = tkinter.tk()
+# The app title
+window = tkinter.Tk()
 window.title('Baking-Chronicles-to-do-list')
+
+# Create a Frame for the Tasks
+task_frame = tkinter.task_frame()
+task_frame.pack()
 
 window.mainloop()
