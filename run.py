7ce7fc69fde8a93210548c3150e74cfd9564fa7b
Graciekan21 <>
2023-12-29 06:04:43 +0000

    import module

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import module
+from tkinter import *
+from tkinter import massagebox
+
+# Function to capture a task
+def capture_task():
+    task = input('Enter your task: ')
+    task_list.append(task)
+    print('Task captured successfully!')
+# The main
+window = tkinter.tk()
+window.title('Baking-Chronicles-to-do-list')
+
+window.mainloop()
