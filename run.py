e10ebce6d810db30054a649eb48826ba367873eb
Graciekan21 <>
2023-12-30 03:47:53 +0000

    add changes to the main

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 #Import module
-import thinter as tk
+import tkinter as tk
 
-# Main window
-window = tk.Tk()
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
 
 # Add title
 title = tk.title(root, text="Baking-chronicles!")
